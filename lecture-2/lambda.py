e5672f19f8c22bf845652ff0f0832a7fcad2170c
wendelsilva <wendel245.ws@gmail.com>
2022-02-14 12:14:06 -0300

    feat(leacture-2): python

---
+++ lecture-2/lambda.py
@@ -0,0 +1,10 @@
+people = [
+    {"name": "Harry", "house": "Gryffindor"},
+    {"name": "Ron", "house": "Gryffindor"},
+    {"name": "Draco", "house": "Slytherin"},
+]
+
+
+people.sort(key=lambda person: person["name"])
+
+print(people)
